3f9629a26f26493442d904456f44d20a96b60728
manurach <36494246+manurach@users.noreply.github.com>
2025-02-10 16:46:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 img = "WIN_20230720_10_52_49_Pro.jpg"
-
+jelly = "S__34308112.jpg"
 st.title("🎈 Junior's App 😁")
 st.image(
     img,
     caption="It's me!",
+    width=800,
+    channels="RGB"
+)
+st.title("🎈 Jelly~~~ 😁")
+st.image(
+    img,
+    caption="It's my pet 🐥!",
     width=800,
     channels="RGB"
 )
