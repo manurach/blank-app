94ce25474ada6db190f2ce0dad0ec901906bb9a5
manurach <36494246+manurach@users.noreply.github.com>
2025-02-10 16:37:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+img = "WIN_20230720_10_52_49_Pro.jpg"
 
 st.title("🎈 Junior's App 😁")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.image(
+    img,
+    caption="It's me!",
+    width=800,
+    channels="RGB"
 )
